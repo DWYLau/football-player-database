a9ff348fffef2a8a391d4c9d6f78a2a342dba8f0
Derek <dereklau15@gmail.com>
2024-09-02 11:34:59 +0900

    Style header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,18 @@ import { Outlet } from "react-router-dom"
 const Home = () => {
   return (
     <div>
-      <div>Home</div>
+      <header>
+        <div className="bg-violet-950 p-3 flex justify-evenly items-center h-[8vh]">
+          <h1 className="text-white p-2">Football Player Database</h1>
+
+          <input
+            type="text"
+            name="value"
+            id="value"
+            placeholder="Search for a player..."
+          />
+        </div>
+      </header>
       <Outlet />
     </div>
   )
